4ccaf271c6e45d797041a76972951e90026554b7
Revelly <ravi.revelly@gmail.com>
2015-10-09 10:19:45 +0530

    added randomNumber generator class with methods

---
+++ ArithmeticQuiz/ArithmeticQuiz/RandomGenerator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArithmeticQuiz
+{
+    public class RandomGenerator
+    {
+        //public RandomGenerator(int startPoint, int endPoint)
+        public RandomGenerator(int startPoint, int endPoint, int noOfRandomCounts)
+        {
+            if (startPoint > endPoint) {
+                throw new Exception("Start point should be less than End Point!");
+            }
+
+            StartPoint = startPoint;
+            EndPoint = endPoint;
+            NoOfRandomCounts = noOfRandomCounts;
+        }
+
+        private int _startPoint;
+        private int _endPoint;
+
+        public int StartPoint { get { return _startPoint; } set { _startPoint = value; } }
+
+        public int EndPoint
+        {
+            get { return _endPoint; }
+            set {
+
+                _endPoint = value;
+                if (_startPoint > value)
+                {
+                    throw new Exception("Start point should be less than End Point!");
+                }
+                
+            }
+            
+        }
+
+        public int getRandomNumber()
+        {
+            Random random = new Random();
+
+            return random.Next(StartPoint, EndPoint);
+        }
+
+        public int NoOfRandomCounts { get; set; }
+
+        public List<int> getRandomNumbers()
+        {
+            List<int> randomNumnbers = new List<int>();
+
+            while (randomNumnbers.Count < NoOfRandomCounts)
+            {
+                generateRandomNumber(randomNumnbers);
+            }
+
+            return randomNumnbers;
+        }
+
+        private void generateRandomNumber(List<int> randomNumnbers)
+        {
+            Random random = new Random();
+
+            var newNumber = random.Next(StartPoint, EndPoint);
+
+            if (!randomNumnbers.Any(n => n == newNumber))
+                randomNumnbers.Add(newNumber);
+        }
+
+        
+    }
+}
---
+++ ArithmeticQuiz/ArithmeticQuizTests/RandomGeneratorTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArithmeticQuiz;
+
+namespace ArithmeticQuizTests
+{
+    /// <summary>
+    ///This is a test class for RandomGeneratorTest and is intended
+    ///to contain all RandomGeneratorTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class RandomGeneratorTests
+    {
+        RandomGenerator target;
+
+        [TestInitialize]
+        public void getTarget()
+        {
+            target = new RandomGenerator(1, 100, 10);
+        }
+
+        [TestMethod]
+        public void TestIfStartPointExists()
+        {
+            Assert.AreNotEqual(0, target.StartPoint);
+        }
+
+        [TestMethod]
+        public void TestIfEndPointExists()
+        {
+            Assert.AreNotEqual(0, target.EndPoint);
+        }
+
+        [TestMethod]
+        public void TestIfNoOfRandomCountsExists()
+        {
+            Assert.AreNotEqual(0, target.NoOfRandomCounts);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestIfStartPointIsLessThanEndPointThrowException()
+        {
+            target.StartPoint = 10;
+            target.EndPoint = 1;
+        }
+
+        [TestMethod]
+        public void TestIfgetRandomNumbersReturnsLengthAsNoOfRandomCounts()
+        {
+            target.NoOfRandomCounts = 10;
+
+            List<int> randomNumbers = target.getRandomNumbers();
+
+            Assert.AreEqual(randomNumbers.Count, target.NoOfRandomCounts);
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void TestVerifyNumberWithinStartAndEndPoint()
+        {
+            int randomNumber = target.getRandomNumber();
+
+            var result = (randomNumber > target.StartPoint ? (randomNumber < target.EndPoint ? true : false) : false);
+            Assert.AreEqual(result, true);
+        }
+
+        
+    }
+}
