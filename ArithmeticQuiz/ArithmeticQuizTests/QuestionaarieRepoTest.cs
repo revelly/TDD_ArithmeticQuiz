23b4c72f89955c05d2d3e6ebeed22dfb7f7504fd
MarkBhethi <b.markandeya@gmail.com>
2015-10-09 11:57:33 +0530

    added repository for saving questions

---
+++ ArithmeticQuiz/ArithmeticQuizTests/QuestionaarieRepoTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArithmeticQuiz;
+
+namespace ArithmeticQuizTests
+{
+    [TestClass]
+    public class QuestionaarieRepoTest
+    {
+        public IQuestionarrieRepository questRepo { get; set; }
+        bool? IsStored = null;
+
+        [TestInitialize]
+        public void TestSetUp()
+        {
+            questRepo = new QuestionarrieRepository();
+            IsStored = false;
+        }
+
+        [TestMethod]
+        public void TestIfquestRepoIsNotNull()
+        {
+            Assert.IsNotNull(questRepo);
+        }
+        [TestMethod]
+        public void TestIfQuestionIsStored()
+        {
+            string question = "1+1=";
+            List<string> options = new List<string>();
+            options.Add("1");
+            options.Add("2");
+            options.Add("3");
+            options.Add("4");
+            string answer = "2";
+            bool IsStored = questRepo.StoreQuestion(question, options, answer);
+            Assert.IsTrue(IsStored);
+
+        }
+
+        
+    }
+}
